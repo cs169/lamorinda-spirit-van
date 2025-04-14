e8d00aab4c87cf58dee0387842e320fd5cda6f4b
AthenaJenkins <athena.j.jenkins@gmail.com>
2025-04-14 16:44:50 -0700

    updated user_authentication_steps to be DRYer

--- features/step_definitions/user_authentication_steps.rb
+++ features/step_definitions/user_authentication_steps.rb
@@ -12,31 +12,23 @@ Given("the following user exists:") do |table|
 end
 
 # features/step_definitions/auth_steps.rb
-Given("a dispatcher is logged in") do
-  @dispatcher = FactoryBot.create(:user,
-                                  :dispatcher,
-                                  email: "dispatcher1@example.com",
-                                  password: "password")
-  visit new_user_session_path
+Given(/^a (.*) is logged in$/) do |user_type|
+  case user_type
+    when "dispatcher"
+      @user = FactoryBot.create(:user,
+                              :dispatcher,
+                              email: "dispatcher1@example.com",
+                              password: "password")
 
-  fill_in "Email", with: @dispatcher.email
-  fill_in "Password", with: @dispatcher.password
-  click_button "Log in"
-
-  expect(page).not_to have_content("Log in")
-  expect(page).to have_title("Lamorinda")
-end
-
-Given("a driver is logged in") do
-  @driver = FactoryBot.create(:driver)
-  @driver_id = @driver.id
-  @user = FactoryBot.create(
-    :user,
-    :driver,
-    email: "driver1@example.com",
-    password: "password"
-  )
-
+    when "driver"
+      @driver = FactoryBot.create(:driver)
+      @driver_id = @driver.id
+      @user = FactoryBot.create(:user,
+                                :driver,
+                                email: "driver1@example.com",
+                                password: "password")
+  end
+    
   visit new_user_session_path
   fill_in "Email", with: @user.email
   fill_in "Password", with: @user.password
@@ -44,5 +36,4 @@ Given("a driver is logged in") do
 
   expect(page).not_to have_content("Log in")
   expect(page).to have_title("Lamorinda")
-  expect(page).to have_current_path(root_path)
 end
