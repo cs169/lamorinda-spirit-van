bf1174daff4ec50dbb9be81612ce189860e173c2
AthenaJenkins <athena.j.jenkins@gmail.com>
2025-04-15 17:10:01 -0700

    updated shift template factory
    updated shift template to take an optional driver_name, if present it builds its driver with it

--- spec/factories/shift_templates.rb
+++ spec/factories/shift_templates.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
   factory :shift_template do
     day_of_week { 1 }
     shift_type { "am" }
-    association :driver
+    transient { driver_name { nil } }
+    driver { if driver_name then association(:driver, name: driver_name) else association(:driver) end }
   end
 end
