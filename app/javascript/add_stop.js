1f57128b4793f74b43ebfe96ae6abeb51132c926
Wilson <wilsonlai@berkeley.edu>
2025-04-20 19:32:03 -0700

    new forms append address-grid container

--- app/javascript/add_stop.js
+++ app/javascript/add_stop.js
@@ -1,22 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
   const addButton = document.getElementById("add-stop-button");
   const deleteButton = document.getElementById("delete-stop-button");
-  const addDestContainer = document.getElementById("add-dest-container");
-  const dest = document.getElementById("destination");
+
+  const dest = document.getElementById("destination-form");
   const space = document.getElementById("col-space")
+  const grid = document.getElementById("address-grid");
+  let stopIndex = 2;
 
   addButton.addEventListener("click", function () {
+
+    console.log(grid.children);
+
     const cloneDest = dest.cloneNode(true);
     const cloneSpace = space.cloneNode(true);
-    addDestContainer.appendChild(cloneDest);
-    addDestContainer.appendChild(cloneSpace);
+
+    grid.appendChild(cloneDest);
+    grid.appendChild(cloneSpace);
   });
 
   deleteButton.addEventListener("click", function () {
     // Removes space and dest clone
-    if (addDestContainer.children.length >= 2) {
-      addDestContainer.removeChild(addDestContainer.lastElementChild);
-      addDestContainer.removeChild(addDestContainer.lastElementChild);
+    console.log(grid.children)
+    if (grid.children.length > 3) {
+      grid.removeChild(grid.lastElementChild);
+      grid.removeChild(grid.lastElementChild);
     }
   });
 
