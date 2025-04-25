2241dcae259a8a9371553fd3c93edf231e701f8f
AthenaJenkins <athena.j.jenkins@gmail.com>
2025-04-25 15:21:44 -0700

    removed unused lines in shift_template model

--- app/models/shift_template.rb
+++ app/models/shift_template.rb
@@ -3,12 +3,4 @@
 class ShiftTemplate < ApplicationRecord
   belongs_to :driver
   validates :day_of_week, :shift_type, presence: true
-
-  def to_date
-    Date.today.beginning_of_week + day_of_week
-  end
-
-  def day_name
-    Date::DAYNAMES[day_of_week]
-  end
 end
