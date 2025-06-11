e0a1f8f30f9fad31ca8aa291f69970efacead38a
XZhaoF <xuefang958@gmail.com>
2025-06-11 04:22:25 -0700

    Fix migration

--- db/migrate/20250315042409_add_unique_index_to_addresses.rb
+++
@@ -1,7 +0,0 @@
-class AddUniqueIndexToAddresses < ActiveRecord::Migration[7.2]
-  def change
-    remove_index :addresses, name: 'index_addresses_on_full_address'
-    
-    add_index :addresses, [:street, :city, :zip_code], unique: true, name: 'index_addresses_on_full_address'
-  end
-end
--- db/migrate/20250611094229_update_addresses_index.rb
+++ db/migrate/20250611094229_update_addresses_index.rb
@@ -1,8 +1,5 @@
 class UpdateAddressesIndex < ActiveRecord::Migration[7.0]
   def change
-    # Remove the old index that only includes street and city
-    remove_index :addresses, name: "index_addresses_on_full_address"
-
     # Add the new index that includes street, city, and zip_code
     add_index :addresses, [:street, :city, :zip_code], unique: true, name: "index_addresses_on_full_address"
   end
